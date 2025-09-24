b1baf99d6358ad4d7beb7016da0a992c6a1f9a1c
travpete <123976124+travpete@users.noreply.github.com>
2025-09-25 00:22:00 +0530

    Add files via upload

---
+++ build/precache-manifest.dacb2db7afd02e1749a742c7db9d750b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f79746f7ea3d2b31a7a96cafc49df6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb66d35d0f1eb47fc542",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "72ac464fafb1b336b76e",
+    "url": "/static/css/main.133d659d.chunk.css"
+  },
+  {
+    "revision": "fb66d35d0f1eb47fc542",
+    "url": "/static/js/2.8216c864.chunk.js"
+  },
+  {
+    "revision": "cbdf9bae47db660bc224",
+    "url": "/static/js/3.607b9e36.chunk.js"
+  },
+  {
+    "revision": "72ac464fafb1b336b76e",
+    "url": "/static/js/main.da6525a6.chunk.js"
+  },
+  {
+    "revision": "68ef532b0b5d432c319a",
+    "url": "/static/js/runtime~main.b5ede664.js"
+  }
+]);
